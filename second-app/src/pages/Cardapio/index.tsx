68e8ab5946f3e89880e1127576547893b189fb76
godoid <godoi-dh@bol.com.br>
2022-12-21 21:49:40 -0300

    Utilizando o classnames como if

--- second-app/src/pages/Cardapio/index.tsx
+++ second-app/src/pages/Cardapio/index.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import Filtros from './Filtros/index';
+
+
+
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
+    const [filtro,setFiltro] = useState<number| null>(null);
     return (
         <main>
             <nav className={styles.menu}>
@@ -22,6 +27,11 @@ export default function Cardapio() {
                 <Buscador
                     busca={busca}
                     setBusca={setBusca} />
+                    <div className={styles.cardapio__filtros}>
+                        
+                        <Filtros filtro={filtro} setFiltro={setFiltro} />
+                        
+                    </div>
             </section>
 
         </main>
