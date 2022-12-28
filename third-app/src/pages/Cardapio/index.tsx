aad8890cd58fc753b03000fbddf5408c36fa6a20
godoid <godoi-dh@bol.com.br>
2022-12-28 15:29:56 -0300

    Reaproveitnado o componente Menu

--- third-app/src/pages/Cardapio/index.tsx
+++ third-app/src/pages/Cardapio/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
 import Filtros from './Filtros/index';
 import Ordenador from './Ordenador/index';
 import Itens from './Itens';
@@ -9,19 +8,16 @@ import Itens from './Itens';
 
 
 
+
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
-  const [filtro,setFiltro] = useState<number| null>(null);
+  const [filtro, setFiltro] = useState<number | null>(null);
   const [ordenador, setOrdenador] = useState('');
   return (
     <main>
-      <nav className={styles.menu}>
-        <Logo />
-
-      </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-                    A casa do código e da massa
+          A casa do código e da massa
 
         </div>
       </header>
@@ -31,15 +27,15 @@ export default function Cardapio() {
           busca={busca}
           setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
-                        
+
           <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador ordenador = {ordenador} setOrdenador = {setOrdenador}/>
-                        
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+
         </div>
-        <Itens 
+        <Itens
           busca={busca}
           filtro={filtro}
-          ordenador = {ordenador}/>
+          ordenador={ordenador} />
       </section>
 
     </main>
