be0d1618e499da89949b86aabbc8e4218b739f86
godoid <godoi-dh@bol.com.br>
2023-01-02 19:14:44 -0300

    CRUD inserido

--- fouth-app/alfood-main/src/paginas/Admin/Restaurantes/AdmnistracaoRestaurantes/FormularioRestaurante/index.tsx
+++ fouth-app/alfood-main/src/paginas/Admin/Restaurantes/AdmnistracaoRestaurantes/FormularioRestaurante/index.tsx
@@ -1,34 +1,55 @@
 import { TextField, Button } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import IRestaurante from '../../../../../interfaces/IRestaurante';
 
 const FormularioRestaurante = () => {
 
-    const [nomeRest, setNomeRest] = useState('');
-    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-        axios.post('http://0.0.0.0:8000/api/v2/restaurantes/',{
-            nome:nomeRest,
-        })
-        .then(()=>{
-            alert('Restaurante cadastrado com sucesso !')
-        })
-        // console.log('preciso enviar dados para api')
-        // console.log('nomeRest')
+  const param = useParams();
+
+  useEffect(() => {
+    if (param.id) {
+      axios.get<IRestaurante>(`http://0.0.0.0:8000/api/v2/restaurantes/${param.id}/`)
+        .then(resposta => setNomeRest(resposta.data.nome))
     }
+  }, [param])
 
+  const [nomeRest, setNomeRest] = useState('');
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
 
-    return (
-        <form onSubmit={aoSubmeterForm}>
-            <TextField
-                value={nomeRest}
-                onChange={evento => setNomeRest(evento.target.value)}
-                id='standar-basic' 
-                label="Nome do Restaurante" 
-                variant="standard" />
-            <Button type='submit' variant='outlined'>Outline</Button>
-        </form>
-    );
+    if(param.id) {
+      axios.put(`http://0.0.0.0:8000/api/v2/restaurantes/${param.id}/`, {
+        nome: nomeRest,
+      })
+        .then(() => {
+          alert('Restaurante atualizado com sucesso !')
+        })
+
+    } else {
+      axios.post('http://0.0.0.0:8000/api/v2/restaurantes/', {
+        nome: nomeRest,
+      })
+        .then(() => {
+          alert('Restaurante cadastrado com sucesso !')
+        })
+      // console.log('preciso enviar dados para api')
+      // console.log('nomeRest')
+    }
+  }
+
+  return (
+    <form onSubmit={aoSubmeterForm}>
+      <TextField
+        value={nomeRest}
+        onChange={evento => setNomeRest(evento.target.value)}
+        id='standar-basic'
+        label="Nome do Restaurante"
+        variant="standard" />
+      <Button type='submit' variant='outlined'>Outline</Button>
+    </form>
+  );
 }
 
 export default FormularioRestaurante
--- fouth-app/alfood-main/src/paginas/Admin/Restaurantes/AdmnistracaoRestaurantes/index.tsx
+++ fouth-app/alfood-main/src/paginas/Admin/Restaurantes/AdmnistracaoRestaurantes/index.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import IRestaurante from '../../../../interfaces/IRestaurante';
-import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { Paper, Button,Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 const AdmnistracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   useEffect(() => {
@@ -9,6 +10,14 @@ const AdmnistracaoRestaurantes = () => {
       .then(resposta => setRestaurantes(resposta.data))
 
   }, [])
+
+  const excluirRest = (restExcluido:IRestaurante) =>{
+    axios.delete(`http://0.0.0.0:8000/api/v2/restaurantes/${restExcluido.id}/`)
+    .then(()=>{
+      const listaRest = restaurantes.filter(rest => rest.id !== restExcluido.id)
+      setRestaurantes([...listaRest])
+    })
+  }
   return (
     // Retornar a lista de restaurante
 
@@ -19,6 +28,12 @@ const AdmnistracaoRestaurantes = () => {
             <TableCell>
               Nome
             </TableCell>
+            <TableCell>
+              Editar
+            </TableCell>
+            <TableCell>
+              Excluir
+            </TableCell>
           </TableRow>
 
         </TableHead>
@@ -27,6 +42,16 @@ const AdmnistracaoRestaurantes = () => {
             <TableRow key={restaurant.id}>
               <TableCell>
                 {restaurant.nome}
+              </TableCell>
+              <TableCell>
+                [<Link to={`/admin/restaurantes/${restaurant.id}`}>editar</Link>]
+              </TableCell>
+              <TableCell >
+                <Button
+                variant='outlined'
+                color="error"
+                onClick={()=> excluirRest(restaurant)}
+                >Excluir</Button>
               </TableCell>
             </TableRow>
           )}
